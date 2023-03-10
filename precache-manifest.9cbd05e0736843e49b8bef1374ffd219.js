97da96dca47012cbbd4e51c2cd12d2075cd2bbdc
spytech-app <spytechapp@gmail.com>
2023-03-10 16:36:55 -0300

    first kick

---
+++ precache-manifest.9cbd05e0736843e49b8bef1374ffd219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbe31233fb9b28b89dc30abd016f128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9f541dcfc60dac5fb24",
+    "url": "/static/css/main.08e682b7.chunk.css"
+  },
+  {
+    "revision": "80e25c8cb559946255d3",
+    "url": "/static/js/2.5489b6f3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5489b6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f541dcfc60dac5fb24",
+    "url": "/static/js/main.35ebb44f.chunk.js"
+  },
+  {
+    "revision": "46742b02a600baac0759",
+    "url": "/static/js/runtime-main.5daac129.js"
+  },
+  {
+    "revision": "ee1766d624a45f18fe824395840611df",
+    "url": "/static/media/logo.ee1766d6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0f326f0dcd83ae19542df1c319ddd8bd.js"
+  "/precache-manifest.9cbd05e0736843e49b8bef1374ffd219.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
